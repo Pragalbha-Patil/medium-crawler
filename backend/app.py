5f9609eed8c79b11b28221f49e698924d9a97b6a
Pragalbha Patil <44872076+Pragalbha-Patil@users.noreply.github.com>
2021-03-09 20:13:52 +0530

    database test api ready

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,51 @@
 from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+
+import datetime; 
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'crawler'
+
+mysql = MySQL(app)
+
 @app.route('/test', methods=['GET'])
-def index():
-    return jsonify({"data": "Test API"})
+def test_db():
+    ct = datetime.datetime.now() 
+    search_status = insert_search_history("Sci", ct, "1")
+    tag_status = insert_tags("Painting")
+    blog_status = insert_blog("Title", "PSP", "Details", "SCi", "wow", 1337.0001)
+    return jsonify({"data": "Test API", "status": [search_status, tag_status, blog_status]})
+
+def insert_tags(tag):
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO tags(tag) VALUES (%s)", (tag,))
+    row_count = cur.rowcount;
+    mysql.connection.commit()
+    cur.close()
+    if(row_count): return 'Success'
+    else: return 'Fail'
+
+def insert_blog(title, author, details, tags, comments, time_taken):
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO blogs(title, author, details, tags, comments, time_taken) VALUES (%s,%s,%s,%s,%s,%s)", (title, author, details, tags, comments, time_taken,))
+    row_count = cur.rowcount;
+    mysql.connection.commit()
+    cur.close()
+    if(row_count): return 'Success'
+    else: return 'Fail'
+
+def insert_search_history(search_tag, created_at, result_found):
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO search_history(search_tag, created_at, result_found) VALUES (%s,%s,%s)", (search_tag, created_at, result_found,))
+    row_count = cur.rowcount;
+    mysql.connection.commit()
+    cur.close()
+    if(row_count): return 'Success'
+    else: return 'Fail'
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
