d0b4b6b12e4d51bcde117de7c7d73f974f40b62c
Pragalbha Patil <44872076+Pragalbha-Patil@users.noreply.github.com>
2021-03-09 19:26:36 +0530

    test api added

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/test', methods=['GET'])
+def index():
+    return jsonify({"data": "Test API"})
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
