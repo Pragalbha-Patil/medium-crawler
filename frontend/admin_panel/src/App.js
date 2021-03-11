91497e342d9c6fe47eef840fedaf7cc1c3a53c6e
Pragalbha Patil <44872076+Pragalbha-Patil@users.noreply.github.com>
2021-03-11 19:53:17 +0530

    Update App.js

--- frontend/admin_panel/src/App.js
+++ frontend/admin_panel/src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Default from "./components/Default";
 import Admin from './components/Admin';
 
